3484245700c563ca5f63f65415642a7a5e0cdd6e
giovanna2005lb <gikalaturague@gmail.com>
2025-04-25 08:45:45 -0300

    Adicionei ID ao personagem e itens

--- src/main/java/com/TavernGuild/demo/model/Personagem.java
+++ src/main/java/com/TavernGuild/demo/model/Personagem.java
@@ -1,11 +1,17 @@
 package com.TavernGuild.demo.model;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.Data;
 
 @Entity
 @Data
 public class Personagem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     private String nome;
     private ClassType classtype;
     private int nivel;
