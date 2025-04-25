427d49b88f6e960b8afb698e4a4d71a933fdcc84
laviniapark <laviniasoo03@gmail.com>
2025-04-25 09:30:58 -0300

    Controller Criado + Problema no BD

---
+++ src/main/java/com/TavernGuild/demo/controller/PersonagemController.java
@@ -0,0 +1,59 @@
+package com.TavernGuild.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import com.TavernGuild.demo.model.Personagem;
+import com.TavernGuild.demo.repository.PersonagemRepository;
+
+@RestController
+@RequestMapping("/characters")
+public class PersonagemController {
+    
+    @Autowired
+    private PersonagemRepository repository;
+
+    @GetMapping
+    public List<Personagem> getAll() {
+        return repository.findAll();
+    }
+
+    @GetMapping("{id}")
+    public Personagem get(@PathVariable Long id) {
+        return getPersonagem(id);
+    }
+
+    @PostMapping
+    public ResponseEntity<Personagem> create(@RequestBody Personagem personagem){
+        repository.save(personagem);
+        return ResponseEntity.status(201).body(personagem);
+    }
+
+    @PutMapping ("{id}")
+    public Personagem update(@PathVariable Long id, @RequestBody Personagem personagem) {
+        personagem.setId(id);
+        return repository.save(personagem);
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable Long id) {
+        repository.delete(getPersonagem(id));
+    }
+
+    private Personagem getPersonagem(Long id) {
+        return repository.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND));
+    }
+
+}
