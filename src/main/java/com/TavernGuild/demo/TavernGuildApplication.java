918bb56bdccd96208031913377fa3d23fe6de519
giovanna2005lb <gikalaturague@gmail.com>
2025-04-25 08:14:22 -0300

    Projeto Inicial

---
+++ src/main/java/com/TavernGuild/demo/TavernGuildApplication.java
@@ -0,0 +1,13 @@
+package com.TavernGuild.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TavernGuildApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TavernGuildApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TavernGuild/demo/TavernGuildApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TavernGuild.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TavernGuildApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
